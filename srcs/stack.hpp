11bfe0e880eff536bdd97e6a4333809c319da144
Adrian <adrian199909@gmail.com>
2022-02-18 17:23:17 +0100

    stack done

---
+++ srcs/stack.hpp
@@ -0,0 +1,86 @@
+#include "./vector.hpp"
+#include "./utils/iterator_traits.hpp"
+
+namespace ft {
+
+	template<class T, class Container = ft::vector<T> >
+	class stack {
+		
+		public:
+			typedef Container	container_type;
+			typedef T			value_type;
+			typedef size_t		size_type;
+
+			stack(const container_type &ctnr = container_type()) {
+				this->_container = ctnr;
+			}
+
+			bool empty() const
+			{
+				return this->_container.empty();
+			}
+
+			size_type size() const
+			{
+				return this->_container.size();
+			}
+
+			value_type &top()
+			{
+				return this->_container.back();
+			}
+
+			const value_type &top() const
+			{
+				return this->_container.back();
+			}
+
+			void push(const value_type &val)
+			{
+				this->_container.push_back(val);
+			}
+
+			void pop()
+			{
+				this->_container.pop_back();
+			}
+
+		protected:
+			container_type _container;
+
+  	friend bool operator==(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{
+		return lhs._container == rhs._container;
+	}
+
+  	friend bool operator!=(const stack<T, Container>& lhs, const stack<T,Container>& rhs)
+	{
+		return lhs._container != rhs._container;
+	}
+
+	friend bool operator<(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{
+		return lhs._container < rhs._container;
+	}
+
+	friend bool operator<=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{
+		return lhs._container <= rhs._container;
+	}
+
+	friend bool operator> (const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{
+		return lhs._container > rhs._container;
+	}
+
+  	friend bool operator>=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{
+		return lhs._container >= rhs._container;
+	}
+	};
+
+
+
+
+}
+
